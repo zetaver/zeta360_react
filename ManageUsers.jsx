16c7bd658bd5cad607e4f4db71fed77d96cdf4a1
zaingillani <abdullahgillaniv204@gmail.com>
2024-12-04 20:40:48 +0500

    .

---
+++ ManageUsers.jsx
@@ -0,0 +1,19 @@
+
+
+import React from 'react';
+
+function ManageUsers() {
+  return (
+    <div className="content-header">
+      <div className="container-fluid">
+        <div className="row mb-2">
+          <div className="col-sm-6">
+            <h1 className="m-0">Admin ManageUsers</h1>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default ManageUsers;
---
+++ Settings.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+function Settings() {
+  return (
+    <div className="content-header">
+      <div className="container-fluid">
+        <div className="row mb-2">
+          <div className="col-sm-6">
+            <h1 className="m-0">Admin Reports</h1>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Settings;
